8bc66a71223141edf627db7c264789c490512d38
Talisa Erazo <talisaerazo@gmail.com>
2018-11-21 02:28:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,17 @@
 def reverse_each_word(string)
-  
+  array = string.split(" ")
+  new_array = []
+  string.collect do |word|
+    word.reverse << new_array
+  end
 end
+
+str = "a,b,c"
+ => "a,b,c" 
+
+array = str.split(/,/)
+ => ["a", "b", "c"] 
+
+ toppings.collect do |topping|
+    puts "I love #{topping} on my burgers!"
+  end
